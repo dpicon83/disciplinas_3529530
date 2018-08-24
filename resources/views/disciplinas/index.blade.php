bd3093489aef8552c357a81959076d33069147da
Daniel Picon <dpicon@sc.usp.br>
2018-08-24 16:06:15 -0300

    FIXE #5 - Edição de disciplina

--- resources/views/disciplinas/index.blade.php
+++ resources/views/disciplinas/index.blade.php
@@ -1,9 +1,11 @@
+ <a href="/disciplinas/create"> Nova Disciplina </a>
+
 <ul>
 	@foreach($disciplinas as $disciplina)
 	<li> 
 		<a href="/disciplinas/{{ $disciplina->id }}">
 			{{ $disciplina->titulo}}
-		</a>
+		</a> - <a href="/disciplinas/{{ $disciplina->id }}/edit"> Editar </a>
 	</li>
 	@endforeach
 </ul>
